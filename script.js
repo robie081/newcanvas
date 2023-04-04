aa17cdcc742b59827d5f9e9847db76f46516905d
robie081 <robert.lantus@icloud.com>
2023-04-04 12:40:31 +0300

    added philosophy

--- script.js
+++ script.js
@@ -10,4 +10,21 @@ document.getElementById("myNav").style.width = "0%";
 }
 
 
+let index = 1;
+myShow(index);
+
+function currentSlide(n) {
+    myShow(index = n);
+}
+
+function myShow(n) {
+    console.log(`n ${n}`);
+    console.log(`index ${index}`);
+    let slides = document.getElementsByClassName("mySlides");
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[index - 1].style.display = "block";
+}
 
