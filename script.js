87422bd655415aa881e1247b8ec6dfdbb41561a8
robie081 <robert.lantus@icloud.com>
2023-12-02 16:04:19 +0200

    last check 1

--- script.js
+++ script.js
@@ -21,10 +21,16 @@ function myShow(n) {
     console.log(`n ${n}`);
     console.log(`index ${index}`);
     let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
 
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     slides[index - 1].style.display = "block";
+
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" activation", "");
+    }
+    dots[index - 1].className += " activation";
 }
 
